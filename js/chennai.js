633f1c649a925f3fba2eaa32d35c1db4351c7a1f
Aruna S <aruna@mapbox.com>
2015-12-02 17:07:17 +0530

    Use new API to select and de-select roads

--- js/chennai.js
+++ js/chennai.js
@@ -1,5 +1,6 @@
 var API_BASE = 'http://chennai.makeamap.in/';
-var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/chennaiflood/cihofezsx00k1u2m4dm7yx57p/';
+var DATASET_ID = 'cihofezsx00k1u2m4dm7yx57p';
+var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/chennaiflood/' + DATASET_ID +"/";
 // var selectedRoadsSource;
 var datasetsAccessToken = 'sk.eyJ1IjoiY2hlbm5haWZsb29kIiwiYSI6ImNpaG9mOGljdTBibmN0aGo3NWR6Y3Q0aXQifQ.X73YugnJDlhZEhxz2X86WA';
 // Define map locations
@@ -70,99 +71,90 @@ map.addControl(new mapboxgl.Navigation());
 
 // Define a layer collection for easy styling
 var mapLayerCollection = {
-  "water": ["water", "waterway-river-canal", "waterway-small"],
-  "road-bridges": ["bridge-main", "bridge-street", "bridge-trunk", "bridge-motorway"],
-  "cartodem": ["chennai-cartodem"],
-  "buildings": ["building"],
-  "road-subways": ["tunnel-motorway", "tunnel-trunk", "tunnel-main", "tunnel-street"],
-  "chennai-relief-camps": ["chennai-relief-camps"],
-  "chennai-relief-camps-22nov": ["chennai-relief-camps-22nov"],
-  "chennai-water-logged-points": ["chennai-water-logged-points"],
-  "road": [
-  'road-main',
-  'road-construction',
-  'road-rail',
-  'road-motorway',
-  'road-trunk',
-  'road-street',
-  'road-service-driveway',
-  'road-path',
-  'tunnel-motorway',
-  'tunnel-trunk',
-  'tunnel-main',
-  'tunnel-street',
-  'bridge-main',
-  'bridge-street',
-  'bridge-trunk',
-  'bridge-motorway',
-  'road-street_limited'
+    "water": ["water", "waterway-river-canal", "waterway-small"],
+    "road-bridges": ["bridge-main", "bridge-street", "bridge-trunk", "bridge-motorway"],
+    "cartodem": ["chennai-cartodem"],
+    "buildings": ["building"],
+    "road-subways": ["tunnel-motorway", "tunnel-trunk", "tunnel-main", "tunnel-street"],
+    "chennai-relief-camps": ["chennai-relief-camps"],
+    "chennai-relief-camps-22nov": ["chennai-relief-camps-22nov"],
+    "chennai-water-logged-points": ["chennai-water-logged-points"],
+    "road": [
+    'road-main',
+    'road-construction',
+    'road-rail',
+    'road-motorway',
+    'road-trunk',
+    'road-street',
+    'road-service-driveway',
+    'road-path',
+    'tunnel-motorway',
+    'tunnel-trunk',
+    'tunnel-main',
+    'tunnel-street',
+    'bridge-main',
+    'bridge-street',
+    'bridge-trunk',
+    'bridge-motorway',
+    'road-street_limited'
   ]
 };
 
 map.on('style.load', function(e) {
-  var chennaiGeoJSON;
-  
-  map.addSource('terrain-data', {
-    type: 'vector',
-    url: 'mapbox://mapbox.mapbox-terrain-v2'
-  });
+    var chennaiGeoJSON;
+    
+    map.addSource('terrain-data', {
+      type: 'vector',
+      url: 'mapbox://mapbox.mapbox-terrain-v2'
+    });
 
-  map.addLayer({
-    "id": "terrain-data",
-    "type": "line",
-    "source": "terrain-data",
-    "source-layer": "contour",
-    "layout": {
-      "line-join": "round",
-      "line-cap": "round"
-    },
-    "paint": {
-      "line-color": "#ff69b4",
-      "line-opacity": "0.3",
-      "line-width": 1
-    }
-  });
-  
-  // Select flooded roads
-  $('#map').toggleClass('loading');
-  var featuresGeoJSON = {
-    'type': 'FeatureCollection',
-    'features': []
-  };
-  function getFeatures(startID) {
-    var url = DATASETS_BASE + 'features';
-    var params = {
-      'access_token': datasetsAccessToken
-    };
-    if (startID) {
-      params.start = startID;
-    }
-    $.getJSON(url, params, function(data, status) {
-      if (data.features.length > 0) {
-        data.features.forEach(function(feature) {
-          feature.properties.id = feature.id;
-        });
-        featuresGeoJSON.features = featuresGeoJSON.features.concat(data.features);
-        var lastFeatureID = data.features[data.features.length - 1].id;
-        getFeatures(lastFeatureID);
-      } else {
-        console.log('features', featuresGeoJSON);
-        $('#map').toggleClass('loading');
-        playWithMap(featuresGeoJSON);
+    map.addLayer({
+      "id": "terrain-data",
+      "type": "line",
+      "source": "terrain-data",
+      "source-layer": "contour",
+      "layout": {
+        "line-join": "round",
+        "line-cap": "round"
+      },
+      "paint": {
+        "line-color": "#ff69b4",
+        "line-opacity": "0.3",
+        "line-width": 1
       }
     });
-  }
+  
+  // Select flooded roads
+    $('#map').toggleClass('loading');
+    var featuresGeoJSON = {
+        'type': 'FeatureCollection',
+        'features': []
+    };
+    function getFeatures(startID) {
+        var url = DATASETS_BASE + 'features';
+        var params = {
+            'access_token': datasetsAccessToken
+        };
+        if (startID) {
+            params.start = startID;
+        }
+        $.getJSON(url, params, function(data, status) {
+            if (data.features.length > 0) {
+                data.features.forEach(function(feature) {
+                    feature.properties.id = feature.id;
+                });
+                featuresGeoJSON.features = featuresGeoJSON.features.concat(data.features);
+                var lastFeatureID = data.features[data.features.length - 1].id;
+                getFeatures(lastFeatureID);
+            } else {
+                console.log('features', featuresGeoJSON);
+                $('#map').toggleClass('loading');
+                playWithMap(featuresGeoJSON);
+            }
+        });
+    }
   getFeatures(null);
-  // $.get(API_BASE + 'places.geojson', function(data, status) {
-  //   chennaiGeoJSON = JSON.parse(data);
-  //   updateFeatureCount(chennaiGeoJSON);
-  //   playWithMap(chennaiGeoJSON);
-  //   $('#map').toggleClass('loading');
-  // })
-  // .fail(function() {
-  //   alert('We might be facing server issues due to high traffic.  If this continues please report here: \nhttps://github.com/osm-in/flood-map/issues/');
-  // });
-
+  
   //Live query
   map.on('mousemove', function(e) {
     map.featuresAt(e.point, {
@@ -183,174 +175,134 @@ map.on('style.load', function(e) {
     });
   });
 
-  // Popups on click
+  //Popups on click
   map.on('click', function(e) {
-    map.featuresAt(e.point, {
-      radius: 10,
-      layer: ['chennai-relief-camps', 'chennai-relief-camps-22nov'],
-      includeGeometry: true
-    }, function(err, features) {
-      if (err) throw err;
+      map.featuresAt(e.point, {
+          radius: 10,
+          layer: ['chennai-relief-camps', 'chennai-relief-camps-22nov'],
+          includeGeometry: true
+      }, function(err, features) {
+          if (err) throw err;
 
-      if (features.length > 0) {
-        var popupHTML = "<h5>" + features[0].properties.Name + "</h5><p>" + $("[data-map-layer=" + features[0].layer.id + "]").html() + "</p>";
-        var popup = new mapboxgl.Popup()
-        .setLngLat(features[0].geometry.coordinates)
-        .setHTML(popupHTML)
-        .addTo(map);
-      }
-    });
+          if (features.length > 0) {
+              var popupHTML = "<h5>" + features[0].properties.Name + "</h5><p>" + $("[data-map-layer=" + features[0].layer.id + "]").html() + "</p>";
+              var popup = new mapboxgl.Popup()
+              .setLngLat(features[0].geometry.coordinates)
+              .setHTML(popupHTML)
+              .addTo(map);
+          }
+      });
   });
 
   // Update map legend from styles
   $("[data-map-layer]").each(function(e) {
+      // Get the color of the feature from the map
+      var obj = $(this).attr('data-map-layer');
 
-    // Get the color of the feature from the map
-    var obj = $(this).attr('data-map-layer');
-
-    try {
-      var color = map.getPaintProperty(obj, 'circle-color');
-      // Set the legend color
-      $(this).prepend('<div class="map-legend-circle" style="background:' + array2rgb(color) + '"></div>');
-    } catch (e) {
-      return;
-    }
+      try {
+          var color = map.getPaintProperty(obj, 'circle-color');
+          // Set the legend color
+          $(this).prepend('<div class="map-legend-circle" style="background:' + array2rgb(color) + '"></div>');
+      } catch (e) {
+          return;
+      }
   });
 
   function playWithMap(data) {
-    var addedRoads = [];
-    var addedFeatures = [];
-    var deletedRoads = [];
+      addedRoads = [];
+      addedFeatures = [];
+      deletedRoads = [];
 
-    for (var i = 0; i < data.features.length; i++) {
-      addedRoads.push(data.features[i].properties.osm_id);
-      addedFeatures.push(data.features[i]);
-    }
-
-    var selectedRoadsSource = new mapboxgl.GeoJSONSource({
-      'data': data
-    });
-
-    map.addSource('selected-roads', selectedRoadsSource);
-    map.addLayer({
-      'id': 'selected-roads',
-      'type': 'line',
-      'source': 'selected-roads',
-      'paint': {
-        'line-color': 'rgba(255,5,230,1)',
-        'line-width': 10,
-        'line-opacity': 0.3
+      for (var i = 0; i < data.features.length; i++) {
+        addedRoads.push(data.features[i].properties.osm_id);
+        addedFeatures.push(data.features[i]);
       }
-    });
+      console.log(addedRoads.length);
+      console.log(addedFeatures.length);
 
-    // Toggle way selection on click
-    map.on('click', function(e) {
-      console.log('clicked on map');
-      if (map.getZoom() >= 15) {
-        map.featuresAt(e.point, {
-          radius: 5,
-          includeGeometry: true,
-          layer: mapLayerCollection["road"]
-        }, function(err, features) {
-          if (err) throw err;
-          console.log('features', features);
-          for (var i = 0; i < features.length; i++) {
-            console.log('inside for loop');
-            var tempObj = {
-              'type': 'Feature'
-            };
-            var index = addedRoads.indexOf(features[i].properties.osm_id);
+      var selectedRoadsSource = new mapboxgl.GeoJSONSource({
+        'data': data
+      });
 
-            if (index !== -1 && addedFeatures[index] && (addedFeatures[index].geometry.coordinates.length === features[i].geometry.coordinates.length)) {
+      map.addSource('selected-roads', selectedRoadsSource);
+      map.addLayer({
+        'id': 'selected-roads',
+        'type': 'line',
+        'source': 'selected-roads',
+        'interactive':true,
+        'paint': {
+          'line-color': 'rgba(255,5,230,1)',
+          'line-width': 10,
+          'line-opacity': 0.3
+        }
+      });
 
-              //Create temp object
-              tempObj.id = features[i].properties.id;
-              tempObj.geometry = features[i].geometry;
-              tempObj.properties = features[i].properties;
-              tempObj.properties["is_flooded"] = false;
-              console.log('tempObj', tempObj);
-              $('#map').toggleClass('loading');
-              var saveURL = DATASETS_BASE + '/features/' + tempObj.id + '?access_token=' + datasetsAccessToken;
-              // console.log('save url', saveURL);
-              $.ajax({
-                'method': 'DELETE',
-                'url': saveURL,
-                'contentType': 'application/json',
-                'success': function() {
-                  data['features'].splice(index, 1);
-                  addedRoads.splice(index, 1);
-                  addedFeatures.splice(index, 1);
-                  selectedRoadsSource.setData(data);
-                  updateFeatureCount(data);
-                }
+      // Toggle way selection on click
+      map.on('click', function(e) {
+          console.log('clicked on map');
+          if (map.getZoom() >= 15) {
+              map.featuresAt(e.point, {radius: 5, includeGeometry: true, layer: 'selected-roads'}, function(err, features) {
+                  console.log("featuresAt callback");
+                  if (err) throw err;
+                
+                  if(features.length > 0){
+                      console.log('inside for loop');
+                      var tempObj = {
+                        'type': 'Feature'
+                      };
+                      $('#map').toggleClass('loading');
+                      var saveURL = DATASETS_BASE + 'features/' + features[0].properties..id + '?access_token=' + datasetsAccessToken;
+                      // console.log('save url', saveURL);
+                      $.ajax({
+                          'method': 'DELETE',
+                          'url': saveURL,
+                          'contentType': 'application/json',
+                          'success': function() {
+                              $('#map').toggleClass('loading');
+                              data["features"] = data['features'].splice(0, 1);
+                              addedRoads = addedRoads.splice(0, 1);
+                              addedFeatures = addedFeatures.splice(0, 1);
+                              selectedRoadsSource.setData(data);
+                              updateFeatureCount(data);
+                          }
+                      });
+                } else {
+                      map.featuresAt(e.point, { radius: 5, includeGeometry: true, layer: mapLayerCollection["road"] }, function(err, glFeatures) {
+                          var tempObj = {
+                              'type': 'Feature'
+                          };
+                          
+                          tempObj.geometry = glFeatures[0].geometry;
+                          tempObj.properties = glFeatures[0].properties;
+                          tempObj.properties["is_flooded"] = true;
+                          $('#map').toggleClass('loading');
+                          var id = md5(JSON.stringify(tempObj));
+                          tempObj.id = id;
+                          var saveURL = DATASETS_BASE + 'features/' + id + '?access_token=' + datasetsAccessToken;
+
+                          $.ajax({
+                              'method': 'PUT',
+                              'url': saveURL,
+                              'data': JSON.stringify(tempObj),
+                              'dataType': 'json',
+                              'contentType': 'application/json',
+                              'success': function(response) {
+                                  $('#map').toggleClass('loading');
+                                  tempObj.id = response.id;
+                                  tempObj.properties.id = response.id;
+                                  addedFeatures.push(tempObj);
+                                  data.features.push(tempObj);
+                                  addedRoads.push(glFeatures[0].properties.osm_id);
+                                  selectedRoadsSource.setData(data);
+                                  updateFeatureCount(data);                  
+                              }
+                          });
+                        }
+                    });
+                  }
               });
-              // $.post(saveURL, JSON.stringify(tempObj), function(data, status) {
-              //   $('#map').toggleClass('loading');
-              // })
-              // .fail(function() {
-              //   alert("Oops! Our server is out for a walk, try again after some time.");
-              // })
-              // .success(function() {
-              //     //Remove from all arrays
-              //     data['features'].splice(index, 1);
-              //     addedRoads.splice(index, 1);
-              //     addedFeatures.splice(index, 1);
-              //     selectedRoadsSource.setData(data);
-              //     updateFeatureCount(data);
-              //   });
-              break;
-
-            } else if (features[i].layer['type'] === 'line' && (features[i].geometry['type'] === 'LineString' ||
-              features[i].geometry['type'] === 'MultiLineString')) {
-
-              //Create temp object
-              tempObj.geometry = features[i].geometry;
-              tempObj.properties = features[i].properties;
-              tempObj.properties["is_flooded"] = true;
-              console.log('new tempObj', tempObj);
-              $('#map').toggleClass('loading');
-              var id = md5(JSON.stringify(tempObj));
-              // tempObj.id = id;
-              var saveURL = DATASETS_BASE + 'features/' + id + '?access_token=' + datasetsAccessToken;
-
-              $.ajax({
-                'method': 'PUT',
-                'url': saveURL,
-                'data': JSON.stringify(tempObj),
-                'dataType': 'json',
-                'contentType': 'application/json',
-                'success': function(response) {
-                  tempObj.id = response.id;
-                  tempObj.properties.id = response.id;
-                  addedFeatures.push(tempObj);
-                  data.features.push(tempObj);
-                  addedRoads.push(features[i].properties.osm_id);
-                  selectedRoadsSource.setData(data);
-                  updateFeatureCount(data);                  
-                }
-              });
-              // $.post(API_BASE + "save", {
-              //   'data': JSON.stringify(tempObj)
-              // }, function(data, status) {
-              //   $('#map').toggleClass('loading');
-              // })
-              // .fail(function() {
-              //   alert("Oops! Our server is out for a walk, try again after some time.");
-              // })
-              // .success(function() {
-              //     //Push to all arrays
-              //     addedFeatures.push(tempObj);
-              //     data.features.push(tempObj);
-              //     addedRoads.push(features[i].properties.osm_id);
-              //     selectedRoadsSource.setData(data);
-              //     updateFeatureCount(data);
-              //   });
-              break;
-            }
           }
-        });
-    }
-  });
+      });
   }
   //Update feature count
   function updateFeatureCount(data) {
@@ -362,20 +314,20 @@ map.on('style.load', function(e) {
 
 
 function array2rgb(color) {
-  // Combine and return the values
-  return 'rgba(' + color.map(function(x) {
-    return x * 255;
-  }).join() + ')';
+    // Combine and return the values
+    return 'rgba(' + color.map(function(x) {
+        return x * 255;
+    }).join() + ')';
 }
 
 $(function() {
-  $("#sidebar").mCustomScrollbar({
-    theme:"rounded-dots",
-    scrollInertia: 100,
-    callbacks: {
-      onInit: function() {
-        $("#sidebar").css("overflow", "auto");
-      }
-    }
-  });
+    $("#sidebar").mCustomScrollbar({
+        theme:"rounded-dots",
+        scrollInertia: 100,
+        callbacks: {
+            onInit: function() {
+                $("#sidebar").css("overflow", "auto");
+            }
+        }
+    });
 });

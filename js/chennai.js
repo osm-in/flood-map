33f9daf9161409afd26e098212f785111a1fb534
arunasank <aruna@mapbox.com>
2017-11-04 21:08:12 +0530

    move constants to a separate file

--- js/chennai.js
+++ js/chennai.js
@@ -1,12 +1,3 @@
-var DATASET_ID = 'ciwm29xyd00082tmocm5l6osb';
-var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/chennaiflood/' + DATASET_ID + '/';
-var DATASETS_ACCESS_TOKEN = 'sk.eyJ1IjoiY2hlbm5haWZsb29kIiwiYSI6ImNpaG9mOGljdTBibmN0aGo3NWR6Y3Q0aXQifQ.X73YugnJDlhZEhxz2X86WA';
-var PUBLIC_ACCESS_TOKEN = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
-var STYLESHEET = 'mapbox://styles/planemad/cih4qzr0w0012awltzvpie7qa';
-var MAP_LAYERS;
-var MAP_LOCATIONS;
-var SELECTED_ROADS_SOURCE;
-
 // Simple map
 mapboxgl.accessToken = PUBLIC_ACCESS_TOKEN;
 var map = new mapboxgl.Map({
---
+++ js/config.js
@@ -0,0 +1,6 @@
+var DATASET_ID = 'ciwm29xyd00082tmocm5l6osb';
+var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/chennaiflood/' + DATASET_ID + '/';
+var DATASETS_ACCESS_TOKEN = 'sk.eyJ1IjoiY2hlbm5haWZsb29kIiwiYSI6ImNpaG9mOGljdTBibmN0aGo3NWR6Y3Q0aXQifQ.X73YugnJDlhZEhxz2X86WA';
+var PUBLIC_ACCESS_TOKEN = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
+var STYLESHEET = 'mapbox://styles/planemad/cih4qzr0w0012awltzvpie7qa';
+var SELECTED_ROADS_SOURCE;

fb63e8606aecc64550a9344a2a174a1f10b2afd0
Arun Ganesh <arun.planemad@gmail.com>
2023-12-04 18:50:36 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
-var DATASET_ID = 'ciwm29xyd00082tmocm5l6osb';
+var DATASET_ID = 'clpqxs1d31ym31umqlq5yaop5';
 var DATASETS_BASE = 'https://api.mapbox.com/datasets/v1/chennaiflood/' + DATASET_ID + '/';
-var DATASETS_ACCESS_TOKEN = 'sk.eyJ1IjoiY2hlbm5haWZsb29kIiwiYSI6ImNpaG9mOGljdTBibmN0aGo3NWR6Y3Q0aXQifQ.X73YugnJDlhZEhxz2X86WA';
+var DATASETS_ACCESS_TOKEN = 'sk.eyJ1IjoiY2hlbm5haWZsb29kIiwiYSI6ImNscHF4dXpxdTA0cGMybG84NDIwd3Z6N24ifQ.R40WZ0N2V_C7HgfKOhCPKg';
 var PUBLIC_ACCESS_TOKEN = 'pk.eyJ1IjoicGxhbmVtYWQiLCJhIjoiemdYSVVLRSJ9.g3lbg_eN0kztmsfIPxa9MQ';
 var STYLESHEET = 'mapbox://styles/planemad/cih4qzr0w0012awltzvpie7qa';
 var LOAD_INFO_LAYERS = ['chennai-relief-camps', 'chennai-relief-camps-22nov'];
